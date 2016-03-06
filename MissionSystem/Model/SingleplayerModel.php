ce780e43454eb6afb0f8bc0f5908552b2fbba707
MkNan <MkNan@MkNan-PC>
2016-03-06 14:58:46 +0800

    Remove Model

--- MissionSystem/Model/SingleplayerModel.php
+++ MissionSystem/Model/SingleplayerModel.php
@@ -1,2 +1,13 @@
 <?php
+
+class SingleplayerModel extends Model {
+    
+    function SelectMissionAndPoint() {
+
+        $sql = "SELECT `MainTableID`, `MainTableName` FROM `Kuas_ERP_C_MainTable` where `MainTableID`>0";
+        $stmt = $this->cont->prepare($sql);
+        $count = $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
 ?>
