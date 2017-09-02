1efb8eb4cf16d199d9514adac0ce41f42112913f
1101137142 <1101137142@gm.kuas.edu.tw>
2017-09-02 15:58:40 +0800

    Update Model.php

--- Model/Model.php
+++ Model/Model.php
@@ -12,7 +12,6 @@ abstract class Model {
         $db_host = 'db.mis.kuas.edu.tw';
         $db_user = 's1101137142';
         $db_name = 's1101137142';
-                                                                                                                                                                                                                                                    $db_password = 'zxc74102';
         $dsn = "mysql:host=$db_host;dbname=$db_name;charset=utf8";
         try {
             $this->cont = new PDO($dsn, $db_user, $db_password);
